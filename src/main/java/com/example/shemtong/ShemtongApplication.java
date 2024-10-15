50431b9acf3a1aca2dc531a67d70e9e17f42134b
socury <socury1104@gmail.com>
2024-10-15 09:16:16 +0900

    basic code

---
+++ src/main/java/com/example/shemtong/ShemtongApplication.java
@@ -0,0 +1,13 @@
+package com.example.shemtong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShemtongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShemtongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shemtong/ShemtongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shemtong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShemtongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
